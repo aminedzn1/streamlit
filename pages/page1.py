b803b4c7b2307f9b249e00479c68d6b127debd54
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-10 12:57:15 +0000

    added boolean value function

--- pages/page1.py
+++ pages/page1.py
@@ -7,5 +7,15 @@ obj_data = st.session_state['obj_data']
 accomplished = obj_data[obj_data.columns[8:]]
 
 array_accomplished = accomplished.to_numpy().flatten()
-array_accomplished = array_accomplished[array_accomplished.astype(str) != '<NA>']
+def boolean_values(x) :
+    match x :
+        case 'Yes' :
+            return(1)
+        case 'No' :
+            return(0)
+        case _ : 
+            return(0)
+
+array_accomplished = array_accomplished.apply(lambda x : boolean_values(x))
+
 st.write(f"{array_accomplished.astype(int).mean()*100}%")
