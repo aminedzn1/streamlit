7873126f867d4798bc922c7b8d481490fa7726bf
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-12 07:56:50 +0000

    test for st_pages

--- streamlit.py
+++ streamlit.py
@@ -4,6 +4,16 @@ st.set_page_config(
      page_title = 'Hello test',
      page_icon = '',
 )
+from st_pages import Page, show_pages, add_page_title
+
+# Specify what pages should be shown in the sidebar, and what their titles and icons
+# should be
+show_pages(
+    [
+        Page("streamlit.py", "Home", "🏠"),
+        Page("pages/modifier.py", "Page 2", ":lower_left_ballpoint_pen:"),
+    ]
+)
 st.title('MAin page')
 
 import pandas as pd
