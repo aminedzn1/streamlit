80b5695f24c31694efd215c3bbe764cc6e2cb136
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-12 12:20:48 +0000

    Try except for metric columns

--- pages/Metrique.py
+++ pages/Metrique.py
@@ -12,7 +12,10 @@ except :
 ###########################################################################################################
 
 domain = st.selectbox('Fonction / Domaine', ("Global Status","1","1A","1C","1G","1I","1P","1S","1T","1V","1Y","1Z","B","S","P","Q"))
-accomplished = obj_data[obj_data.columns[domain]]
+try :    
+    accomplished = obj_data[obj_data.columns[domain]]
+except :
+    accomplished = obj_data[obj_data.columns[8:]]
 
 array_accomplished = accomplished.to_numpy().flatten()
 def boolean_values(X) :
