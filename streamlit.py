612ac4a11894a94d49ecc1edf9e5ea9c90766fd9
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-12 08:32:32 +0000

    test i don't even know how this works

--- streamlit.py
+++ streamlit.py
@@ -16,7 +16,7 @@ obj_data = obj_data[:39]
 
 categories = ['Yes','No', 'N/A']
 obj_data["Global Status"] = (
-    obj_data["#"].astype("category").cat.remove_categories(obj_data['#']).cat.add_categories(categories)
+    obj_data["Global Status"].astype("category").cat.remove_categories(obj_data['Global Status']).cat.add_categories(categories)
 )
 
 st.write("objectives")
