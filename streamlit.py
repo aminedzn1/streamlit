b4be81eae306725d25ef3339e6e12a27c2c3d5ae
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-12 08:17:35 +0000

    try expect for saving the dataset changes

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,10 @@ st.title('MAin page')
 import pandas as pd
 import numpy as np
 
-obj_data = pd.read_csv(r'Objectives 2024 - 1 & DOA - 2024 objective status EOY.csv', dtype = 'string',keep_default_na=False)
+try :
+    obj_data = pd.read_csv(r'/mount/src/streamlit/new_obj_data.csv')
+except :
+    obj_data = pd.read_csv(r'Objectives 2024 - 1 & DOA - 2024 objective status EOY.csv', dtype = 'string',keep_default_na=False)
 obj_data.columns = obj_data.iloc[0]
 obj_data = obj_data.drop(0)
 obj_data = obj_data[:39]
