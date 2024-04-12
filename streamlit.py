d712de14bcf30d9f981135cc14a3256317e9339f
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-12 08:26:41 +0000

    trying things

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,7 @@ st.title('MAin page')
 import pandas as pd
 import numpy as np
 
-obj_data = pd.read_csv(r'Objectives 2024 - 1 & DOA - 2024 objective status EOY.csv', dtype = 'string',keep_default_na=False)
+obj_data = pd.read_csv(r'data/Objectives 2024 - 1 & DOA - 2024 objective status EOY.csv', dtype = 'string',keep_default_na=False)
 obj_data.columns = obj_data.iloc[0]
 obj_data = obj_data.drop(0)
 obj_data = obj_data[:39]
