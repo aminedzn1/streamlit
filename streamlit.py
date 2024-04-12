8cd55ec0bceaa6d38aa10779fe90f84a0262781a
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-12 08:43:49 +0000

    typo correction

--- streamlit.py
+++ streamlit.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 
 obj_data = pd.read_csv(r'data/Objectives 2024 - 1 & DOA - 2024 objective status EOY.csv', dtype = 'string',keep_default_na=False)
-obj_data.columns = obj_data["Objective category","Source","#","Objective","KPI / Achievement criteria","Owners/ Applicability","Target date","Global Status","1","1A","1C","1G","1I","1P","1S","1T","1V","1Y","1Z","B","S","P","Q"]
+obj_data.columns = ["Objective category","Source","#","Objective","KPI / Achievement criteria","Owners/ Applicability","Target date","Global Status","1","1A","1C","1G","1I","1P","1S","1T","1V","1Y","1Z","B","S","P","Q"]
 obj_data = obj_data[:39]
 
 categories = ['Yes','No', 'N/A']
