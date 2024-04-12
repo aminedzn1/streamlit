bc81f11712ef0ea02b72577533cba67acb180d01
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-12 08:12:02 +0000

    error unknown

--- pages/Modifier.py
+++ pages/Modifier.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.set_page_config(
      page_title = 'Modifier',
-     page_icon = 'lower_left_ballpoint_pen',
-     multi_app_icon = 'lower_left_ballpoint_pen'
+     page_icon = 'lower_left_ballpoint_pen'
 )
