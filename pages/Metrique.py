dfb225da31481785aabe5e2b74fce818b373a090
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-15 09:12:56 +0000

    comments added to the top

--- pages/Metrique.py
+++ pages/Metrique.py
@@ -81,6 +81,6 @@ except :
 comment = st.text_input("Comment")
 
 if st.button("Post") :
-    comments.append(comment)
+    comments += comment
     st.session_state.comments = comments
 st.table(pd.DataFrame(comments, columns = ["Comments"]))
