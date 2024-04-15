a7e18fc13663681204db7ae42e8b972c3109581e
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-15 09:44:37 +0000

    global -> objective

--- home.py
+++ home.py
@@ -3,24 +3,6 @@ import pandas as pd
 import numpy as np
 
 ###########################################################################################################
-st.set_page_config(
-     page_title = 'Home page',
-     page_icon = 'house',
-)
-st.title('2024 SMS in 1 & DOA Objectives')
-###########################################################################################################
-
-try :
-    obj_data = st.session_state.obj_data
-except : 
-    obj_data = pd.read_csv(r'data/Objectives 2024 - 1 & DOA - 2024 objective status EOY.csv', dtype = 'string',keep_default_na=False)
-
-###########################################################################################################
-
-
-obj_data.columns = ["Objective category","Source","#","Objective","KPI / Achievement criteria","Owners/ Applicability","Target date","Global Status","1","1A","1C","1G","1I","1P","1S","1T","1V","1Y","1Z","B","S","P","Q"]
-filtered = st.multiselect("Filter columns", options=list(obj_data.columns), default=['Objective','Global Status']) 
-st.write(obj_data[filtered])
-
+st.title("1 & DOA SMS Dashboard")
 
 ###########################################################################################################
