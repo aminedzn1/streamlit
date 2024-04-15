32512f5ae4d6c6882a22ab916f6389a7a72cf900
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-15 13:23:49 +0000

    final objective markdown

--- pages/objectives_metriques.py
+++ pages/objectives_metriques.py
@@ -88,13 +88,3 @@ if st.button("Post") :
     comments = [comment] + comments
     st.session_state.comments = comments
 st.table(pd.DataFrame(comments, columns = ["Comments"]))
-
-import streamlit as st
-import numpy as np
-
-original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Original image</p>'
-st.markdown(original_title, unsafe_allow_html=True)
-
-
-new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">New image</p>'
-st.markdown(new_title, unsafe_allow_html=True)
