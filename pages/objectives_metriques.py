49c1c1b7b8e8f2384f0781bbb365afe85802265c
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-15 12:24:01 +0000

    test talel

--- pages/objectives_metriques.py
+++ pages/objectives_metriques.py
@@ -47,23 +47,25 @@ array_accomplished = np.array(pd.Series(array_accomplished).apply(lambda x : boo
 array_filled = np.array(pd.Series(array_filled).apply(lambda x : filled_values(x)))
 ###########################################################################################################
 col1, col2 = st.columns(2)
-with col1 : 
-    value = round(array_accomplished.mean()*100, 2)
-    if value < 50 : 
-        with open (r'styles/metric_red.css') as f:
-            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-            st.metric("Objective %", value)
-    elif value > 50 and value < 85 : 
-        with open (r'styles/metric_amber.css') as f:
-            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-            st.metric("Objective %", value)
-    elif value > 85 and value <= 100 : 
-        with open (r'styles/metric_green.css') as f:
-            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-            st.metric("Objective %", value)
+with col1 :
+        #value = round(array_accomplished.mean()*100, 2)
+        value = 80
+        if value < 50 : 
+            with open (r'styles/metric_red.css') as f:
+                st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+                st.metric("Objective %", value)
+        elif value > 50 and value < 85 : 
+            with open (r'styles/metric_amber.css') as f:
+                st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+                st.metric("Objective %", value)
+        elif value > 85 and value <= 100 : 
+            with open (r'styles/metric_green.css') as f:
+                st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+                st.metric("Objective %", value)
 
 with col2 :
-    value = round(array_filled.mean()*100, 2)
+    #value = round(array_filled.mean()*100, 2)
+    value = 30
     if value < 50 : 
         with open (r'styles/metric_red.css') as f:
             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
