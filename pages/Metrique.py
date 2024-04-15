460c77935e93fb8d367ff4971ac04bc5a4e6154c
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-15 09:04:59 +0000

    columns for filled percent

--- pages/Metrique.py
+++ pages/Metrique.py
@@ -14,7 +14,7 @@ obj_data.columns = ["Objective category","Source","#","Objective","KPI / Achieve
 
 ###########################################################################################################
 
-domain = st.selectbox('Fonction / Domaine', ("Global Status","1","1A","1C","1G","1I","1P","1S","1T","1V","1Y","1Z","B","S","P","Q"))
+domain = st.selectbox('Fonction / Domaine', ("All","Global Status","1","1A","1C","1G","1I","1P","1S","1T","1V","1Y","1Z","B","S","P","Q"))
 
 try :    
     accomplished = obj_data[domain]
@@ -22,7 +22,16 @@ except :
     accomplished = obj_data[obj_data.columns[8:]]
 
 array_accomplished = accomplished.to_numpy().flatten()
+array_filled = accomplished.to_numpy().flatten()
+
 array_accomplished = array_accomplished[array_accomplished != 'N/A']
+array_filled = array_filled[array_filled !='N/A']
+def filled_values(X) : 
+    if X == "1" or X == "0" :
+        return (1)
+    else : 
+        return (0)
+    
 def boolean_values(X) :
     if X == '1' :
         return(1)
@@ -30,26 +39,41 @@ def boolean_values(X) :
         return(0)
 
 array_accomplished = np.array(pd.Series(array_accomplished).apply(lambda x : boolean_values(x)))
-
+array_filled = np.array(pd.Series(array_filled).apply(lambda x : filled_values(x)))
 ###########################################################################################################
+col1, col2 = st.columns(2)
+with col1 : 
+    value = round(array_accomplished.mean()*100, 2)
+    if value < 50 : 
+        with open (r'styles/metric_red.css') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+            st.metric("Objective %", value)
+    elif value > 50 and value < 85 : 
+        with open (r'styles/metric_amber.css') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+            st.metric("Objective %", value)
+    elif value > 85 and value <= 100 : 
+        with open (r'styles/metric_green.css') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+            st.metric("Objective %", value)
 
-value = round(array_accomplished.mean()*100, 2)
-if value < 50 : 
-    with open (r'styles/metric_red.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-        st.metric("Objective %", value)
-elif value > 50 and value < 85 : 
-    with open (r'styles/metric_amber.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-        st.metric("Objective %", value)
-elif value > 85 and value <= 100 : 
-    with open (r'styles/metric_green.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-        st.metric("Objective %", value)
-
+with col2 :
+    value = round(array_filled.mean()*100, 2)
+    if value < 50 : 
+        with open (r'styles/metric_red.css') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+            st.metric("Objective filled %", value)
+    elif value > 50 and value < 85 : 
+        with open (r'styles/metric_amber.css') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+            st.metric("Objective filled %", value)
+    elif value > 85 and value <= 100 : 
+        with open (r'styles/metric_green.css') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+            st.metric("Objective filled %", value)
 ###########################################################################################################
 st.subheader('Add comment', divider = "red" )
- 
+
 try : 
     comments = st.session_state.comments
 except :    
