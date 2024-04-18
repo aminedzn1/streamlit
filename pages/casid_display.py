ba113a0a5e34519f4d2b0ddce6034f183b59ebd7
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-18 08:45:14 +0000

    a

--- pages/casid_display.py
+++ pages/casid_display.py
@@ -19,5 +19,14 @@ casid_data = pd.read_csv(r'data/CASID status - CASID Status .csv', dtype = 'stri
 filtered = st.multiselect("Filter columns", options=list(casid_data.columns), default=['Supplier','CoC','Current status']) 
 st.write(casid_data[filtered])
 
-
+col1, col2 = st.columns(2)
+with col1 :
+    st.write(casid_data['Current status'].value_counts())
+with col2 :    
+    domain = st.selectbox(label = 'Function / Domain', options = casid_data['CoC'].unique())
+    try :     
+        domain_casid_data = casid_data.loc[casid_data['CoC'] == domain]
+        st.write(domain_casid_data['Current status'].value_counts())
+    except :
+        pass 
 ###########################################################################################################
