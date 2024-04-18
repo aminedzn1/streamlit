e70c25d80f97acac608d118a3f612804525b3851
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-18 11:24:30 +0000

    mt-09

---
+++ pages/pps_display.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+###########################################################################################################
+st.set_page_config(
+     page_title = 'ePPS display',
+     page_icon = 'clipboard',
+)
+st.title('PPS mock up')
+###########################################################################################################
+
+pps_data = pd.read_csv(r'data/practical_problem_solving_filtered', dtype = 'string',keep_default_na=False)
+
+st.write(pps_data.head(3))
+
+col1, col2 = st.columns(2)
+
+with col1 : 
+     st.write('Assessement on time')
+     st.markdown(f'<p style="font-family:Arial; color:Red; font-size: 30px;">44 %</p>', unsafe_allow_html=True)
+     st.write('Acknowledgement on time')
+     st.markdown(f'<p style="font-family:Arial; color:Green; font-size: 30px;">89 %</p>', unsafe_allow_html=True)
+
+with col2 : 
+     st.write('Number of open SMS cases')
+     st.markdown('<p style = "font-family:Arial; font-size: 40px;">34</p>', unsafe_allow_html= True)
+     st.write('Number of closed SMS cases')
+     st.markdown('<p style = "font-family:Arial; font-size: 40px;">1</p>', unsafe_allow_html= True)
