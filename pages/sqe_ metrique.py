3c53180ac4738565c19254fc0828dbe5a423dad1
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-18 06:46:06 +0000

    a

--- pages/sqe_ metrique.py
+++ pages/sqe_ metrique.py
@@ -36,7 +36,8 @@ sqe_data['activeBool'] =sqe_data['SQE Active?'].apply(lambda x : boolean_values(
 
 sqe_count_domain = sqe_data[['Domain/Function', 'activeBool']].groupby(['Domain/Function']).sum()
 st.write(sqe_count_domain)
-st.write(sqe_count_domain['activeBool'] > 0)
+
+value = f"{sum(np.array(sqe_count_domain['activeBool']) > 0)}/{len(sqe_count_domain['activeBool'])}"
 st.metric("SQE number", value)
 ###########################################################################################################
 
