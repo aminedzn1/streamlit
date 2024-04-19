4b1f04639387207e6229ffda5d8a0a584a6507c4
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-19 08:19:52 +0000

    s

--- home.py
+++ home.py
@@ -11,6 +11,17 @@ st.set_page_config(
      initial_sidebar_state = 'collapsed'
 )
 
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
 st.title("1 & DOA SMS Dashboard")
 
 ###########################################################################################################
