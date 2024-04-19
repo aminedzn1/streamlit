328d78cf00a5f6efc20186d44be3d3d55cf5ec27
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-19 09:33:22 +0000

    &

--- home.py
+++ home.py
@@ -28,7 +28,7 @@ st.title("1 & DOA SMS Dashboard")
 col1, col2, col3 = st.columns([1,2,1])
 
 with col1:
-    st.markdown('div[data-testid="stContainer"] > div:first-of-type {background-color: rgb(0,0,102);}') 
+    st.markdown('<style>{div[data-testid="stContainer"] > div:first-of-type {background-color: rgb(0,0,102);}}</style>') 
     objectives = st.container()
     objectives.subheader('Objectives')
     col11, col12 = objectives.columns(2)
