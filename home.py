bf5a50bb7a5e83115362857c55e299a686e6a162
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-19 09:57:55 +0000

    s

--- home.py
+++ home.py
@@ -7,8 +7,8 @@ import numpy as np
 st.set_page_config(
      page_title = '1 & DOA SMS Dashboard',
      page_icon = 'barchart',
-     layout = 'wide',
-     initial_sidebar_state = 'collapsed'
+     layout = 'wide'#,
+     #initial_sidebar_state = 'collapsed'
 )
 
 st.markdown(
