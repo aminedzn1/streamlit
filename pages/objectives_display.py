d148b593b4c2fd0ee25834544edc4c6542f2de83
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-25 11:36:55 +0000

    s

--- pages/objectives_display.py
+++ pages/objectives_display.py
@@ -25,17 +25,13 @@ st.page_link('home.py', label = 'Home', icon = '🏠', use_container_width=True)
 st.title('2024 SMS in 1 & DOA Objectives')
 ###########################################################################################################
 
-try :
-    obj_data = st.session_state.obj_data
-except : 
-    obj_data = pd.read_csv(r'data/Objectives 2024 - 1 & DOA - 2024 objective status EOY.csv', dtype = 'string',keep_default_na=False)
+obj_data = pd.read_csv(r'data/Objectives 2024 - 1 & DOA - 2024 objective status EOY.csv', dtype = 'string',keep_default_na=False)
 
 ###########################################################################################################
 
 
-obj_data.columns = ["Objective category","Source","#","Objective","KPI / Achievement criteria","Owners/ Applicability","Target date","Global Status","1","1A","1C","1G","1I","1P","1S","1T","1V","1Y","1Z","B","S","P","Q"]
 filtered = st.multiselect("Filter columns", options=list(obj_data.columns), default=['Objective','Global Status']) 
-st.dataframe(obj_data[filtered],use_container_width=True)
+st.table(obj_data[filtered],use_container_width=True)
 
 
 
