1582c8871f9aa3c77879a529ea5fd89810b63f2c
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-04-26 07:54:34 +0000

    s

--- pages/budget_metrique.py
+++ pages/budget_metrique.py
@@ -97,7 +97,7 @@ target = go.Scatter(
       marker=dict(symbol = 'cross-thin',color="green"),
       name = 'Target (k€)'
 )
-spending = px.bar(
+spending = go.bar(
     x=x,
     y=[y_2,y_3],
     name = ['Actuals (k€)','Commitment (k€)']
