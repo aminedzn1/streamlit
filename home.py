7de04ae524956529b9f7ac28da1151ca027e14d7
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-05-14 09:49:27 +0000

    s

--- home.py
+++ home.py
@@ -206,7 +206,7 @@ col1, col2, col3 = st.columns([1,2,1])
 with col1:
     st.subheader('Objectives')
     with stylable_container(key = 'metric_container',
-                            css_styles = container_style, ) :
+                            css_styles = container_style, on_click = switch_page('objectives metriques')) :
         objectives = st.container()
 #    col11, col12 = objectives.columns(2)
 #    with col11 :
