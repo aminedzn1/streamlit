488e5c31ede9445795f3d70402c76287f1c1049d
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-05-17 08:54:59 +0000

    s

--- pages/budget_metrique.py
+++ pages/budget_metrique.py
@@ -98,7 +98,7 @@ fig = px.bar(
     x='Domain/Function',
     y=['Actuals (k€)','Commitment (k€)']
 )
-fig.update_traces(marker_color=['#ADD8E6','#000370'])
+fig.update_traces(marker_color = '#000370')
 fig.add_trace(go.Scatter(
       x=x,
       y=y_1,
