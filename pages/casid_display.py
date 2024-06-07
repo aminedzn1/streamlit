a3cab28174c133e779f7c5ee6226701fa781ced5
aminedzn1 <145107904+aminedzn1@users.noreply.github.com>
2024-06-07 08:20:39 +0000

    s

--- pages/casid_display.py
+++ pages/casid_display.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(
-     page_title = 'CASID Status',
+     page_title = 'DOA Concern upload file',
      page_icon = 'bar_chart',
      layout = 'wide',
      initial_sidebar_state = 'collapsed'
@@ -22,17 +22,47 @@ st.markdown(
 
 ###########################################################################################################
 
-st.page_link('home.py', label = 'Home', icon = '🏠', use_container_width=True)
-st.title('1 & DOA CASID Status')
-###########################################################################################################
-
-casid_data = pd.read_csv(r'data/CASID status - CASID Status .csv', dtype = 'string',keep_default_na=False)
-
-###########################################################################################################
+st.title('DOA Concern follow-up file upload')
+df = pd.read_csv(st.file_uploader('Upload .csv file'))
 
 
+df['Opening Date'] = pd.to_datetime(df['Opening Date'])
+df = df.loc[df['Opening Date'] >= pd.to_datetime('22/05/2023')]
+df2 = df[[
+       'Opening Date',
+       'Title',
+       'T0\n (message sent to the DAN and the SMS Officer )',
+       'T1 \n(Date of\nSystemic Safety Impact \nStatus Confirmation)',
+       'Systemic Safety Impact\n\nYes / No', 
+       'SMS Officer',
+       'e-PPS Ref.\n(SMS or not SMS)',
+       'T2 \n(Date of\nSystemic Safety Topic\nClosure)',
+       'T0\n (message sent to the DAN and the SMS Officer ).1',
+       'T1 \n(Date of\nSystemic Safety Impact \nStatus Confirmation).1',
+       'Systemic Safety Impact\n\nYes / No.1',
+       'SMS Officer.1',
+       'e-PPS Ref.\n(SMS or not SMS).1',
+       'T2 \n(Date of\nSystemic Safety Topic\nClosure).1'
+       ]]
 
-filtered = st.multiselect("Filter columns", options=list(casid_data.columns), default=['Supplier','CoC','Current status']) 
-st.write(casid_data[filtered])
- 
+cols2 = []
+for col in df2.columns :
+    if col[len(col)-2] == '.' :
+        cols2.append(col)
+
+for i in df2.index : 
+    if type(df2.loc[i,'SMS Officer.1']) == float :
+        df2.loc[i,'Systemic Safety Impact\n\nYes / No.1'] = 'N/A'
+    else :
+        if df2.loc[i,'Systemic Safety Impact\n\nYes / No.1'] != 'Yes' and df2.loc[i,'Systemic Safety Impact\n\nYes / No.1'] != 'No':
+            df2.loc[i,'Systemic Safety Impact\n\nYes / No.1'] = 'Under review'
+
+for i in df2.index : 
+    if df2.loc[i,'Systemic Safety Impact\n\nYes / No'] != 'Yes' and df2.loc[i,'Systemic Safety Impact\n\nYes / No'] != 'No':
+            df2.loc[i,'Systemic Safety Impact\n\nYes / No'] = 'Under review'
+
+st.bar_chart(df2['Systemic Safety Impact\n\nYes / No.1'].value_counts())
+st.bar_chart(df2['Systemic Safety Impact\n\nYes / No'].value_counts())
+dfinal = df2[['Title', 'Systemic Safety Impact\n\nYes / No','SMS Officer','Systemic Safety Impact\n\nYes / No.1', 'SMS Officer.1']]
+
 
